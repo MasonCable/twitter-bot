3a51fb07a686fa0bf46f92d25c7eb7ddf008e9b2
Mason Cable <masoncablelsc@gmail.com>
2019-11-22 02:42:51 -0800

    still working with the like button

--- main.py
+++ main.py
@@ -29,29 +29,22 @@ class TwitterBot:
         time.sleep(3)
 
     def makePost(self, post):
-        postArea = 'public-DraftStyleDefault-block'
         bot = self.bot
         
-        postInput =  bot.find_element_by_class_name(postArea)
-
+        # Get the page where the tweet is made
+        bot.get('https://twitter.com/compose/tweet')
+        # This is the classname for the compose tweet area
+        postArea = find_element_by_class_name('public-DraftEditor-content')
+        # # We are clicking on the area
+        bot.find_element_by_class_name('public-DraftEditor-content').click()
+        time.sleep(2)
+        # # We are clearing any data that might be there
         postInput.clear()
-        postInput.send_keys(post)
-        postInput.send_keys(Keys.RETURN)
+        # # We are sending the page the post which we received from the parameters of the function
+        postArea.send_keys(post)
+        # We can use this command if we want to space to the next line
+        postInput.send_keys(Keys.CONTROL + Keys.SHIFT + Keys.RETURN)
 
-    # This function was built when I had a problem with being logged out afer the initial link change
-
-    def logBackIn(self):
-        bot = self.bot
-        hashtag = self.hashtag
-        bot.get('https://twitter.com/search?q='+ hashtag +'&src=typed_query')
-        email = bot.find_element_by_name('session[username_or_email]')
-        password = bot.find_element_by_name('session[password]')
-        email.clear()
-        password.clear()
-        email.send_keys(self.username)
-        password.send_keys(self.password)
-        password.send_keys(Keys.RETURN)
-        time.sleep(10)
 
     def likeTweet(self):
         bot = self.bot
@@ -74,53 +67,20 @@ class TwitterBot:
                     bot.get(link)
                     try:
                         # bot.find_element_by_class_name('r-yyyyoo').click()
-                        # bot.find_element_by_xpath("//div[@data-testid='like']").click()
+                        bot.find_element_by_xpath("//*[@id='react-root']/div/div/div/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div/div[1]/div/article/div/div[6]/div[3]/div/div").click()
                         time.sleep(5)
                     except Exception as ex:
                         time.sleep(60)
-                        print(ex)
+                        print('There was an error')
 
-            # for link in links:
-            #     bot.get('https://twitter.com/' + link)
-            #     print(link)
-                # try:
-                #     bot.find_element_by_class_name('HeartAnimation').click()
-                #     time.sleep(10)
-                # except Exception as ex:
-                #     time.sleep(60)
-                #     print(ex)
-        
-    def loginFromHashtag(self):
-        bot = self.bot
-        bot.get('https://twitter.com/search?q=funny&src=typed_query')
-        time.sleep(3)
-        email = bot.find_element_by_class_name('email-input')
-        password = bot.find_element_by_name('session[password]')
-        email.clear()
-        password.clear()
-        email.send_keys(self.username)
-        password.send_keys(self.password)
-        password.send_keys(Keys.RETURN)
 
-        for i in range(1,3):
-            bot.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-            time.sleep(3)
-            print('scrolling')
-            tweets = bot.find_elements_by_class_name('css-1dbjc4n')
-            links = [elem.get_attribute('data-permalink-path') for elem in tweets]
-            for link in links:
-                bot.get('https://twitter.com/' + link)
-                try:
-                    bot.find_element_by_class_name('HeartAnimation').click()
-                    time.sleep(10)
-                except Exception as ex:
-                    time.sleep(60)
-                    print(ex)
+   
 
 mainAccount = TwitterBot('dabbing.developer@gmail.com', 'twitterFaker#6969', 'funny')
 
 
 mainAccount.login()
+# mainAccount.makePost('This is my first tweet from the bot')
 mainAccount.likeTweet()
 
 
